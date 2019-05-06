814b043057b54c5e3fb109b40ef509e188dcd477
Brokkonaut <hannos17@gmx.de>
2019-05-06 06:04:53 +0200

    Fix wg flag registration

--- src/main/java/com/Acrobot/ChestShop/Plugins/WorldGuardFlags.java
+++ src/main/java/com/Acrobot/ChestShop/Plugins/WorldGuardFlags.java
@@ -1,5 +1,6 @@
 package com.Acrobot.ChestShop.Plugins;
 
+import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.protection.flags.Flags;
 import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
@@ -10,7 +11,9 @@ public class WorldGuardFlags {
     static {
         StateFlag enableShop;
         try {
-            enableShop = Flags.register(new StateFlag("allow-shop", false));
+            StateFlag flag = new StateFlag("allow-shop", false);
+            WorldGuard.getInstance().getFlagRegistry().register(flag);
+            enableShop = flag;
         } catch (FlagConflictException | IllegalStateException e) {
             enableShop = (StateFlag) Flags.get("allow-shop");
         }
