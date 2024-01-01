df8ae51eabd8fadfe75d8f9de1d1457eee20c6d7
Maik E <maik@ehemanns.de>
2024-01-01 15:15:30 +0100

    Remove unused code in ItemInfo command class
    The changes in the codebase remove unused imports and commenting out unused code in ItemInfo command class. This process helps in maintaining the code clean and easy to read, improving the maintainability of the code.

--- src/main/java/com/Acrobot/ChestShop/Commands/ItemInfo.java
+++ src/main/java/com/Acrobot/ChestShop/Commands/ItemInfo.java
@@ -15,9 +15,6 @@ import com.Acrobot.Breeze.Utils.StringUtil;
 import com.Acrobot.ChestShop.ChestShop;
 import com.Acrobot.ChestShop.Configuration.Messages;
 import com.Acrobot.ChestShop.Events.ItemInfoEvent;
-import com.Acrobot.ChestShop.Utils.ComponentUtils;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.TextComponent;
 
 /**
  * @author Acrobot
@@ -51,15 +48,6 @@ public class ItemInfo implements CommandExecutor {
             sender.sendMessage("  " + ChatColor.DARK_RED + INCORRECT_ITEM_ID);
             return;
         }
-        // String name = StringUtil.capitalizeFirstLetter(item.getType().name(), '_');
-        String signName = MaterialUtil.getSignName(item);
-        TextComponent tc = new TextComponent("  ");
-        tc.setColor(net.md_5.bungee.api.ChatColor.DARK_GRAY);
-        BaseComponent tcName = ComponentUtils.getLocalizedItemName(item);
-        tcName.setColor(net.md_5.bungee.api.ChatColor.WHITE);
-        tc.addExtra(tcName);
-        tc.addExtra(" " + signName);
-        sender.spigot().sendMessage(tc);
 
         ItemInfoEvent event = new ItemInfoEvent(sender, item);
         ChestShop.callEvent(event);
