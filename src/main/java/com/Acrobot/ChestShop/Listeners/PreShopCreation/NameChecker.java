76053f4c34bc085a36ac3bb5acbdd1d41ef687d2
Maik E <maik@ehemanns.de>
2024-01-01 13:59:19 +0100

    Remove unused import and simplify conditional in NameChecker
    Removed extra import 'Permission' and simplified a conditional statement in the NameChecker class. This refactoring streamlines the logic for setting the name during PreShopCreationEvent, making the code more straightforward without changing functionality.

--- src/main/java/com/Acrobot/ChestShop/Listeners/PreShopCreation/NameChecker.java
+++ src/main/java/com/Acrobot/ChestShop/Listeners/PreShopCreation/NameChecker.java
@@ -5,7 +5,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
-import com.Acrobot.ChestShop.Permission;
 import com.Acrobot.ChestShop.Configuration.Properties;
 import com.Acrobot.ChestShop.Events.PreShopCreationEvent;
 import com.Acrobot.ChestShop.UUIDs.NameManager;
@@ -22,8 +21,7 @@ public class NameChecker implements Listener {
         Player player = event.getPlayer();
         if (Properties.ADMIN_SHOP_NAME.equalsIgnoreCase(name)) {
             name = Properties.ADMIN_SHOP_NAME;
-        }
-        if (name.isEmpty() || (!NameManager.canUseName(player, name) && !Permission.has(player, Permission.ADMIN))) {
+        } else {
             name = NameManager.getNameFor(player);
         }
         event.setOwnerName(name);
